f5ffa45b92d82d82a66a3c00be2369f59542c0f3
ivanp___ <rafa.pejovic@gmail.com>
2023-06-05 22:38:44 +0200

    Created movies controller

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            return View();
+        }
+    }
+}
